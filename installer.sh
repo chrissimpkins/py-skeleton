2944542b057b220f11ded0ce651d035252143f10
Chris Simpkins <git.simpkins@gmail.com>
2016-03-23 14:53:42 -0400

    initial commit

---
+++ installer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo " "
+echo "Pulling the pyapp archive..."
+echo " "
+curl -LO https://github.com/chrissimpkins/pyapp/archive/master.tar.gz
+
+echo " "
+echo "Unpacking the archive..."
+echo " "
+tar -xzvf master.tar.gz
+
+echo " "
+echo "Preparing project files..."
+mv pyapp-master/* .
+mv pyapp-master/.gitignore ./.gitignore
+mv pyapp-master/.landscape.yml ./.landscape.yml
+mv pyapp-master/.travis.yml ./.travis.yml
+
+echo " "
+echo "Cleaning up..."
+rm -rf pyapp-master
+rm master.tar.gz
+rm installer.sh
+
+echo "Complete."
