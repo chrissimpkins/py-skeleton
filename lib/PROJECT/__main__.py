e37bfe672b0582d2c270295201e8810a8648b781
Chris Simpkins <git.simpkins@gmail.com>
2019-08-27 23:28:34 -0400

    add __main__.py with executable source template

---
+++ lib/PROJECT/__main__.py
@@ -0,0 +1,16 @@
+
+
+def main():  # pragma: no cover
+    run(sys.argv[1:])
+
+def run(argv):
+	# ===========================================================
+    # argparse command line argument definitions
+    # ===========================================================
+    parser = argparse.ArgumentParser(
+        description="{{ DESCRIPTION }}"
+    )
+    parser.add_argument(
+        "--version", action="version", version="{{ PROJECT }} v{}".format(__version__)
+    )
+    args = parser.parse_args(argv)
