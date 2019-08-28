4a7888091343d3f24ace03ecb5fb6e6b9525842a
Chris Simpkins <git.simpkins@gmail.com>
2019-08-27 23:30:12 -0400

    major overhaul of setup.py file

--- setup.py
+++ setup.py
@@ -1,63 +1,84 @@
+import io
 import os
-import re
+import sys
 from setuptools import setup, find_packages
 
+# Package meta-data.
+NAME = "{{ PROJECT }}"
+DESCRIPTION = ""
+LICENSE = "Apache License v2.0"
+URL = "{{ URL }}"
+EMAIL = "{{ EMAIL }}"
+AUTHOR = "{{ AUTHOR }}"
+REQUIRES_PYTHON = ">=3.6.0"
 
-def docs_read(fname):
-    return open(os.path.join(os.path.dirname(__file__), 'docs', fname)).read()
+INSTALL_REQUIRES = ["",
+                    ]
+# Optional packages
+EXTRAS_REQUIRES = {
+    # for developer installs
+    "dev": ["coverage", "pytest", "tox", "flake8", "pytype"],
+    # for maintainer installs
+    "maintain": ["wheel", "setuptools", "twine"]
+}
 
+this_file_path = os.path.abspath(os.path.dirname(__file__))
 
-def version_read():
-    settings_file = open(os.path.join(os.path.dirname(__file__), 'lib', '{{PROJECT}}', 'settings.py')).read()
-    major_regex = """major_version\s*?=\s*?["']{1}(\d+)["']{1}"""
-    minor_regex = """minor_version\s*?=\s*?["']{1}(\d+)["']{1}"""
-    patch_regex = """patch_version\s*?=\s*?["']{1}(\d+)["']{1}"""
-    major_match = re.search(major_regex, settings_file)
-    minor_match = re.search(minor_regex, settings_file)
-    patch_match = re.search(patch_regex, settings_file)
-    major_version = major_match.group(1)
-    minor_version = minor_match.group(1)
-    patch_version = patch_match.group(1)
-    if len(major_version) == 0:
-        major_version = 0
-    if len(minor_version) == 0:
-        minor_version = 0
-    if len(patch_version) == 0:
-        patch_version = 0
-    return major_version + "." + minor_version + "." + patch_version
+# Version
+main_namespace = {}
+version_fp = os.path.join(this_file_path, "lib", "{{ PROJECT }}", "__init__.py")
+try:
+    with io.open(version_fp) as v:
+        exec(v.read(), main_namespace)
+except IOError as version_e:
+    sys.stderr.write(
+        "[ERROR] setup.py: Failed to read the version data for the version definition: {}".format(
+            str(version_e)
+        )
+    )
+    raise version_e
 
+# Use repository Markdown README.md for PyPI long description
+try:
+    with io.open("README.md", encoding="utf-8") as f:
+        readme = f.read()
+except IOError as readme_e:
+    sys.stderr.write(
+        "[ERROR] setup.py: Failed to read the README.md file for the long description definition: {}".format(
+            str(readme_e)
+        )
+    )
+    raise readme_e
 
 setup(
-    name='{{PROJECT}}',
-    version=version_read(),
-    description='',
-    long_description=(docs_read('README.rst')),
-    url='',
-    license='MIT license',
-    author='',
-    author_email='',
-    platforms=['any'],
+    name=NAME,
+    version=main_namespace["__version__"],
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    license=LICENSE,
+    platforms=["Any"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    package_dir={"": "lib"},
     packages=find_packages("lib"),
-    package_dir={'': 'lib'},
-    install_requires=['commandlines', 'standardstreams'],
-    entry_points = {
-        'console_scripts': [
-            '{{PROJECT}} = {{PROJECT}}.app:main'
-        ],
-},
-    keywords='',
     include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRES,
+    python_requires=REQUIRES_PYTHON,
+    entry_points={"console_scripts": ["{{ PROJECT }} = {{ PROJECT }}.__main__:main"]},
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
