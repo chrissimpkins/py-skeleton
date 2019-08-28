7c4014a4f6e80e7f7413578e1c43faf05cbef6bf
Chris Simpkins <git.simpkins@gmail.com>
2019-08-27 23:27:59 -0400

    eliminate unnecessary files

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-coverage run --source {{PROJECT}} -m py.test
-coverage report -m
-coverage html
-
-#coverage xml
-#codecov --token=$CODECOV_{{PROJECT}}
--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python3 setup.py sdist bdist_wheel
-twine upload dist/{{project}}-0.1.0*
